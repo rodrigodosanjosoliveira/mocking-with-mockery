a1b8ac4c3c7ad49ca8ee343efdabbc80b30c2548
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2022-08-26 00:08:16 -0300

    test: :white_check_mark: servive for product

---
+++ internal/services/product_service_test.go
@@ -0,0 +1,43 @@
+package services_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/rodrigodosanjosoliveira/mocking-with-mockery/internal/mocks/repomocks"
+	"github.com/rodrigodosanjosoliveira/mocking-with-mockery/internal/models"
+	"github.com/rodrigodosanjosoliveira/mocking-with-mockery/internal/services"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockProductRepository struct {
+	onErr bool
+}
+
+func (r *MockProductRepository) Add(models.Product) error {
+	if r.onErr {
+		return errors.New("error while inserting")
+	}
+	return nil
+}
+
+func TestProductService_Insert(t *testing.T) {
+	repo := &repomocks.ProductRepositoryInterface{}
+	repo.On("Add", mock.AnythingOfType("models.Product")).
+		Return(nil).
+		Once()
+
+	service := services.NewProductService(repo)
+
+	err := service.Insert(
+		"2f1afe98-63c4-4f59-bcaf-1df835602bdb",
+		models.InsertProductDto{
+			Name:  "Macbook",
+			Price: 20500,
+			Stock: 10,
+		},
+	)
+
+	assert.Nil(t, err)
+}
