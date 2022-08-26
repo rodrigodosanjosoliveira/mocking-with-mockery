df35f36b0a6034df043fe712dcdbdadd83034287
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2022-08-26 00:09:16 -0300

    feat: :sparkles: entry point

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+
+	"github.com/rodrigodosanjosoliveira/mocking-with-mockery/internal/models"
+	"github.com/rodrigodosanjosoliveira/mocking-with-mockery/internal/repositories"
+	"github.com/rodrigodosanjosoliveira/mocking-with-mockery/internal/services"
+)
+
+func main() {
+	//creating dependency
+	repo := repositories.NewProductRepository()
+	service := services.NewProductService(&repo)
+
+	// inserting
+	err := service.Insert(
+		"d681753c-dca3-433d-9d8a-0d455d4a3065",
+		models.InsertProductDto{
+			Name:  "Macbook",
+			Price: 27500,
+			Stock: 10,
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// inserting
+	err = service.Insert(
+		"6220d651-2c62-43b9-bf31-2a6f79059f69",
+		models.InsertProductDto{
+			Name:  "HP Laptop",
+			Price: 13500,
+			Stock: 5,
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// inserting
+	err = service.Insert(
+		"77076fae-a4a9-4e73-ad21-7fb89f4ab39d",
+		models.InsertProductDto{
+			Name:  "Iphone 13 pro",
+			Price: 24500,
+			Stock: 25,
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
